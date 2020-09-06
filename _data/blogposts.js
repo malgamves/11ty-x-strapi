ed33104cf00905abe94c04f75905956e43ee7c87
Daniel Phiri <malgamves@gmail.com>
2020-09-06 22:50:43 +0200

    added strapi source

---
+++ _data/blogposts.js
@@ -0,0 +1,87 @@
+const fetch = require("node-fetch");
+
+// get blogposts
+async function getAllBlogposts() {
+  // max number of records to fetch per query
+  const recordsPerQuery = 100;
+
+  // number of records to skip (start at 0)
+  let recordsToSkip = 0;
+
+  // do we make a query ?
+  let makeNewQuery = true;
+
+  // Blogposts array
+  let blogposts = [];
+
+  // make queries until makeNewQuery is set to false
+  while (makeNewQuery) {
+    try {
+      // initiate fetch
+      const data = await fetch("https://okaaaaaaayyy.herokuapp.com/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          query: `{
+              blogs {
+              id
+              Title
+              Description
+              Published
+              Body
+              author {
+                username
+              }
+              Slug
+            }
+          }`,
+        }),
+      });
+
+      // store the JSON response when promise resolves
+      const response = await data.json();
+
+      // handle DatoCMS errors
+      if (response.errors) {
+        let errors = response.errors;
+        errors.map((error) => {
+          console.log(error.message);
+        });
+        throw new Error("Aborting: CMS errors");
+      }
+
+      // update blogpost array with the data from the JSON response
+      blogposts = blogposts.concat(response.data.blogs);
+
+      // prepare for next query
+      recordsToSkip += recordsPerQuery;
+
+      // stop querying if we are getting back less than the records we fetch per query
+      if (response.data.blogs.length < recordsPerQuery) {
+        makeNewQuery = false;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  // format blogposts objects
+  const blogpostsFormatted = blogposts.map((item) => {
+    return {
+      id: item.id,
+      title: item.Title,
+      slug: item.Slug,
+      summary: item.Description,
+      body: item.Body,
+    };
+  });
+
+  // return formatted blogposts
+  return blogpostsFormatted;
+}
+
+// export for 11ty
+module.exports = getAllBlogposts;
